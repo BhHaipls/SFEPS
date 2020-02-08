e4ba49177846d4c8e1176e437c97d20dde90e5f6
BhHaipls <bh.haipls@gmail.com>
2020-02-08 13:41:30 +0300

    Initial project, create skeleton of project

---
+++ src/main/java/ua/haipls/sfeps/SfepsApplication.java
@@ -0,0 +1,13 @@
+package ua.haipls.sfeps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfepsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfepsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/haipls/sfeps/SfepsApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.haipls.sfeps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfepsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
